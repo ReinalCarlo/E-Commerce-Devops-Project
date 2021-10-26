407c806ab09d7301a811bf2458513d90546ca40f
Reinal <reinalcarlo1@gmail.com>
2021-10-27 02:41:26 +0530

    Update

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,13 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCeykKdf3Hgq7EFz8IOUc0SuxR04kGpTi0',
+  authDomain: 'devops-project-110c7.firebaseapp.com',
+  projectId: 'devops-project-110c7',
+  storageBucket: 'devops-project-110c7.appspot.com',
+  messagingSenderId: '101528556057',
+  appId: '1:101528556057:web:75b2436427d619d1a9d088',
+  measurementId: 'G-5702M8G7SY',
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
