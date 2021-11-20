eeb46186f028feb5249911659362b327defe32a6
Reinal <reinalcarlo1@gmail.com>
2021-11-20 17:01:36 +0530

    commit

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,34 @@
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
